8470bd937c3c88a439aa9d0bb670db6079c9577a
Alexander Adetayo <adetayoalexander12@gmail.com>
2026-03-03 07:52:24 +0100

    feat: add dynamic export to enable server-side rendering for CheckoutCallbackPage

--- src/app/(site)/checkout/callback/page.tsx
+++ src/app/(site)/checkout/callback/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { Suspense, useEffect, useRef, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import Link from "next/link";
